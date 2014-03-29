177b58ce55e4d3a9c5fd4b51f97fbfb5e6801b9b
Mateu X. Hunter <hunter@missoula.org>
2014-03-29 20:11:46 +0000

    Import of MATEU/Bracket-1.10 from CPAN.
    gitpan-cpan-distribution: Bracket
    gitpan-cpan-version:      1.10
    gitpan-cpan-path:         MATEU/Bracket-1.10.tar.gz
    gitpan-cpan-author:       MATEU
    gitpan-cpan-maturity:     released

--- lib/Bracket/Controller/Final4.pm
+++ lib/Bracket/Controller/Final4.pm
@@ -112,7 +112,10 @@ sub save_picks : Local {
         }
     }
     $c->stash->{params} = $params;
-    $c->response->redirect($c->uri_for($c->controller('Player')->action_for('home')));
+    $c->response->redirect(
+        $c->uri_for($c->controller('Player')->action_for('home'))
+        . "/${player_id}"
+    );
 
     return;
 }
@@ -154,12 +157,6 @@ sub view : Local {
         }
         else {
             if ($player_pick->game->round >= $player_pick->pick->round_out) {
-
-                #if ($player_pick->game == 63) {
-                warn "round: " . $player_pick->game->round . "\n";
-                warn "round_out: " . $player_pick->pick->round_out . "\n";
-
-                #}
                 $class_for{ $player_pick->game->id } = 'out';
             }
             else {
